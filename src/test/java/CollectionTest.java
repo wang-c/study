4775141becf0d48d7905ca5432e3febb150cdd3d
tujinpeng <2363695612@qq.com>
2017-09-15 13:50:42 +0800

    LinkedHashMap lru study

--- src/test/java/CollectionTest.java
+++ src/test/java/CollectionTest.java
@@ -7,17 +7,20 @@ import java.util.Map;
 public class CollectionTest {
 
     public static void main(String[] args) {
-        Map cache = new LinkedHashMap<Object, Object>() {
+        Map cache = new LinkedHashMap<Object, Object>(16, 0.75f, true) {
             @Override
             protected boolean removeEldestEntry(Map.Entry<Object, Object> eldest) {
-                return size()>5;
+                return size() > 5;
             }
         };
-        cache.put("1",new Object());
-        cache.put("2",new Object());
-        cache.put("3",new Object());
-        cache.put("4",new Object());
-        cache.put("5",new Object());
-        cache.put("6",new Object());
+        cache.put("1", new Object());
+        cache.put("2", new Object());
+        cache.put("3", new Object());
+        cache.put("4", new Object());
+        cache.put("5", new Object());
+        cache.get("1");
+        cache.get("3");
+        cache.put("6", new Object());
+
     }
 }

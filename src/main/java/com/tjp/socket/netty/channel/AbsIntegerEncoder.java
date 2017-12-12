06d18c0d3f7f897d9feafb08c2a9395c71f29d51
tujinpeng <2363695612@qq.com>
2017-12-12 16:20:19 +0800

    netty encode test

---
+++ src/main/java/com/tjp/socket/netty/channel/AbsIntegerEncoder.java
@@ -0,0 +1,21 @@
+package com.tjp.socket.netty.channel;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageEncoder;
+
+import java.util.List;
+
+/**
+ * Created by tujinpeng on 2017/12/12.
+ */
+public class AbsIntegerEncoder extends MessageToMessageEncoder<ByteBuf> {
+
+    @Override
+    protected void encode(ChannelHandlerContext channelHandlerContext, ByteBuf in, List<Object> out) throws Exception {
+        while (in.readableBytes() >= 4) {
+            int value = Math.abs(in.readInt());
+            out.add(value);
+        }
+    }
+}

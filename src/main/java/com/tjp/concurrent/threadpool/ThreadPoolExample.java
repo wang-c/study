845efb62a60c992851965b673c2cf71d8527cf91
tujinpeng <2363695612@qq.com>
2017-12-25 19:39:32 +0800

    thread pool 2017-12-25 study

--- src/main/java/com/tjp/concurrent/threadpool/ThreadPoolExample.java
+++ src/main/java/com/tjp/concurrent/threadpool/ThreadPoolExample.java
@@ -43,21 +43,89 @@ public class ThreadPoolExample {
      * 向线程池提交任务的方式：
      * 1.execute
      * 2.submit
-     * 3。batch submit
+     * 3.group submit
+     *
      * @throws InterruptedException
      */
     @Test
-    public void submitTaskWay() throws InterruptedException {
-        //自定义的线程池
-        ThreadPoolExecutor pool = new ThreadPoolExecutor(2, 2, 0, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>(1), new NamedThreadFactory("tjp-pool", false), new AbortPolicyWithReport());
-
-        //线程池并发处理能力为3
+    public void submitTaskWay() throws InterruptedException, ExecutionException {
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(20, 20, 0, TimeUnit.MINUTES,
+                new LinkedBlockingQueue<Runnable>(1),
+                new NamedThreadFactory("tjp-pool", false),
+                new AbortPolicyWithReport());
+        //1.execute
         pool.execute(new Task("task-1"));
-        Future<?> future = pool.submit(new CallableTask("task-1"));//提交一个有返回值的任务
-        pool.execute(new Task("task-2"));
+         /*
+         * 2.submit future模式:
+         * 返回future 代表未来结果的占位符
+         *
+         * future模式(futureTask实现):
+         *     (1)等待/通知机制实际上是实现了【实现生产者消费者线程之间的通信】,通信共享的是【任务完成的结果】
+         *     (2)等待:调用future.get()方法,若提交的任务未完成则生产者线程进入等待状态(在future的栈中等待)
+         *     (3)通知:由线程池中工作者线程完成任务后通知唤醒生产者线程,返回结果
+         *
+         *-传统的等待通知机制【锁+条件队列】:
+         *-等待线程:
+         *lock.lock();
+         * try{
+         *    while(result==null){//共享变量条件不满足时,生产者线程进入等待
+         *      condition.await();
+         *    }
+         *    //条件满足时,返回结果
+         *    result result;
+         * }finally{
+         *    lock.unLock();
+         * }
+         *-通知线程:
+         * lock.lock()
+         * try{
+         *    result=run()//执行完任务
+         *    condition.signalAll();//通知所有等待着线程,返回结果
+         * }finally{
+         *    lock.unLock();
+         * }
+         *
+         *
+         *-futureTask实现的等待通知机制【(volatile)state+简单栈】:
+         *
+         *-等待线程:
+         * while(state<=COMPLETING){//当任务未执行完时,线程进入stack等待
+         *      awaitStack();
+         * }
+         * if(state==normal)//当条件满足时,返回任务执行结果
+         *    return result;
+         *
+         *-通知线程:
+         * result=callable.call();
+         * UNSAFE.putOrderedInt(this, stateOffset, NORMAL);//cas设置state为执行完成
+         * finishCompletion();//通知栈上等待的所有线程
+         *
+         *future模式的通知等待机制和传统方式的区别?:
+         *(1)future模式相对于传统方式,更加的轻量级
+         *(2)future模式通过对【volatile变量写后读】的原则,保证线程间共享变量【任务执行的结果result】的可见性,
+         *   而传统的模式通过加锁的方式,保证线程间共享变量的可见性
+         *(3)这两者间的区别有点类似乐观锁和悲观锁
+         *
+         */
+        Future<?> future = pool.submit(new CallableTask("task-2"));//提交一个有返回值的任务
+        if (future.get() != null) {//等待task-2任务完成
+            System.out.println("task-2 finish , return : " + future.get());
+        }
+
+        //3.group submit
+        CompletionService completionService = new ExecutorCompletionService(pool);
+        //向线程池提交一组任务
+        for (int i = 0; i < 5; i++) {
+            completionService.submit(new CallableTask("batch-task-" + i));
+        }
+        //让主线程等待所有的任务执行完毕
+        for (int i = 0; i < 5; i++) {
+            completionService.take();//当有一个任务执行完毕后,会加入到completionService阻塞队列中
+        }
+        System.out.println("all batch tasks finish");
 
         //让主线程不要结束
-        Thread.sleep(20000);
+        Thread.sleep(10000);
 
     }
 
@@ -65,11 +133,12 @@ public class ThreadPoolExample {
      * 线程池的生命周期：
      * -状态
      * -终结
+     *
      * @throws ExecutionException
      * @throws InterruptedException
      */
     @Test
-    public  void lifeCyclePool() throws ExecutionException, InterruptedException {
+    public void lifeCyclePool() throws ExecutionException, InterruptedException {
         //定制一个核心线程和最大线程为2 任务队列大小为1的线程池(同一时间最多消费3个任务【两个核心线程 队列1个】)
         ThreadPoolExecutor pool = new ThreadPoolExecutor(2, 2, 0, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>(1), new NamedThreadFactory("tjp-pool", false), new AbortPolicyWithReport());
         //两种方式提交任务
@@ -125,7 +194,7 @@ public class ThreadPoolExample {
         public void run() {
             try {
                 //do somenthing
-                Thread.sleep(5000);//模拟大量很慢的任务
+                Thread.sleep(1000);//模拟大量很慢的任务
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -133,7 +202,7 @@ public class ThreadPoolExample {
         }
     }
 
-    static class CallableTask implements Callable {
+    static class CallableTask implements Callable<String> {
 
         private String name;
 
@@ -142,15 +211,15 @@ public class ThreadPoolExample {
         }
 
         @Override
-        public Object call() throws Exception {
+        public String call() throws Exception {
             try {
                 //do somenthing
-                Thread.sleep(5000);//模拟大量很慢的任务
+                Thread.sleep(1000);//模拟大量很慢的任务
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             System.out.println(Thread.currentThread().getName() + " executed " + name);
-            return name + " executed result";
+            return name + "-result";
         }
 
     }
